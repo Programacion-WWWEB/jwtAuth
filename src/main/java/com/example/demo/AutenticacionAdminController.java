b62ad39392c750def7d4ffa891f8e3a903d3ccfc
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-11-26 10:35:09 -0500

    security

--- src/main/java/com/example/demo/AutenticacionAdminController.java
+++ src/main/java/com/example/demo/AutenticacionAdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AutenticacionAdminController {
 
-        @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+        @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"}, allowedHeaders = "*")
     @PostMapping( value = "/admin/autenticacion-usuario", produces = MediaType.APPLICATION_JSON_VALUE )
     public JWTToken autenticarAdmin( @RequestParam( required = true) String correo, @RequestParam( required = true     ) String contrasena ){
         System.out.println("----------1<<<<<<<>>>>>><-------");
--- src/main/java/com/example/demo/AutenticacionController.java
+++ src/main/java/com/example/demo/AutenticacionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AutenticacionController {
 
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+    @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"}, allowedHeaders = "*")
     @PostMapping( value = "/public/autenticacion-usuario", produces = MediaType.APPLICATION_JSON_VALUE )
     public JWTToken autenticar( @RequestParam( required = true) String correo, @RequestParam( required = true     ) String contrasena ){
         System.out.println("----------1<<<<<<<>>>>>><-------");
